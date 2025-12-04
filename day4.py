31924976532b49b79ceeab5fadf367664f5d2825
Habuon <59359193+Habuon@users.noreply.github.com>
2025-12-04 07:56:53 +0100

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,133 @@
+import requests
+import time
+from abc import ABC, abstractmethod
+
+
+class Clock:
+    def __init__(self):
+        self.start_time = 0
+    def __enter__(self):
+        self.start_time = time.time()
+    def __exit__(self, *args, **kwargs):
+        stop_time = time.time()
+        print(f"[i] Took {int(stop_time - self.start_time) // 60} min. and {int(stop_time - self.start_time) % 60}s in total")
+
+
+class AocMeta(ABC):
+
+	def __init__(self, day, cookies):
+		self.session = requests.Session()
+		self.day = day		
+		
+		requests.utils.add_dict_to_cookiejar(self.session.cookies, cookies)
+	
+	
+	@abstractmethod
+	def process_input(self, inp: str) -> dict:
+		...
+		   
+		    
+	@abstractmethod
+	def first_star(self, inp):
+		...
+		    
+		    
+	@abstractmethod
+	def second_star(self, inp):
+		...
+	
+	def get_input(self) -> dict:
+		if self.session is None:
+			raise ValueError("Improperly initialized")
+			
+		resp = self.session.get(f"https://adventofcode.com/2025/day/{self.day}/input")
+		result = resp.content.decode()
+		result = self.process_input(result)
+		return result
+
+	def submit_answer(self, level, answer):
+		while True:
+		    resp = self.session.post(f"https://adventofcode.com/2025/day/{self.day}/answer", data={"level": level, "answer": answer})
+		    content = resp.content.decode()
+		    if "That's not the right answer" in content:
+		        return False
+		    if "You gave an answer too recently" in content:
+		        print("[*] Response sent too soon after each other")
+		        time.sleep(30)
+		        continue
+		    return True
+		
+		
+	def run(self):
+		inp = self.get_input()
+		first_answer = self.first_star(inp)
+		correct = self.submit_answer(level=1, answer=first_answer)
+		print(f"[*] First star result: {first_answer} ... {'correct' if correct else 'incorrect'}")
+		if not correct:
+			return False
+			
+		time.sleep(10)
+		
+		second_answer = self.second_star(inp)
+		correct = self.submit_answer(level=2, answer=second_answer)
+		print(f"[*] Second star result: {second_answer} ... {'correct' if correct else 'incorrect'}")
+		
+		return correct
+
+
+class Solution(AocMeta):
+	def _get_surroundings(self, row, col):
+		return {(row + dr, col + dc) for dr in range(-1, 2) for dc in range(-1, 2)}
+
+	def process_input(self, inp: str) -> dict:
+		result = [list(line) for line in inp.split("\n")]
+		paper_rolls = [(row, col) for row in range(len(result)) for col in range(len(result[row])) if result[row][col] == "@"]
+		return {"diagram": result, "paper_rolls": set(paper_rolls)}
+		
+		   
+	def first_star(self, inp):
+		result = 0
+		diagram, paper_rolls = inp["diagram"], inp["paper_rolls"]
+		for paper_roll in paper_rolls:
+			row, col = paper_roll
+			surroundings = self._get_surroundings(row, col)
+			intersections = surroundings.intersection(paper_rolls)
+			number_of_intersections = len(intersections) - 1
+			if number_of_intersections < 4:
+				result += 1
+		return result
+
+
+	def second_star(self, inp):
+		result = 0
+		diagram, paper_rolls = inp["diagram"], inp["paper_rolls"]
+		while True:
+			to_remove = set()
+			for paper_roll in paper_rolls:
+				row, col = paper_roll
+				surroundings = self._get_surroundings(row, col)
+				intersections = surroundings.intersection(paper_rolls)
+				number_of_intersections = len(intersections) - 1
+				if number_of_intersections < 4:
+					result += 1
+					to_remove.add(paper_roll)
+			paper_rolls = paper_rolls - to_remove
+			if len(to_remove) == 0:
+				break
+		return result
+
+
+
+def main():
+	day = "4"
+	cookies = {"session": "<SESSION_ID>"}
+	
+	solution = Solution(day=day, cookies=cookies)
+		
+	with Clock():
+		solution.run()
+
+
+
+if __name__ == "__main__":
+    main()
